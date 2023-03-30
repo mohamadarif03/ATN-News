ba18d68d9577c20f8c95fa60531a838d7309e499
mohamadarif03 <pecintarealmadrid2@gmail.com>
2023-03-30 07:57:18 +0700

    Uakeh

---
+++ app/Models/keywoard.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class keywoard extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tag';
+    protected $fillable = ['tag', 'berita_id'];
+
+    public function berita()
+    {
+        return $this->belongsTo(berita::class,'id');
+    }
+}
+
--- resources/views/category/berita isi/index.blade.php
+++ resources/views/category/berita isi/index.blade.php
@@ -25927,16 +25927,10 @@
                                                                 <div class="ubio description-text">
                                                                         <div class="d-flex">
                                                                             {{ $row->komentar }}
-                                                                        
-                                                                             <!-- Button trigger modal -->
-                                                                                <!-- Modal -->
-                                                                            
-                                                                                {{-- <a href="" data-bs-toggle="modal" data-bs-target="#exampleModal"> --}}
                                                                                 @if (Route::has('login'))
     
                                                                                 @auth
                                                                                 <div id="" class="balaskomen" data-id="balas-{{$row->id}}" style="margin-left: 100px; display:flex; cursor: pointer;" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                                                                    
                                                                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-reply" viewBox="0 0 16 16">
                                                                                         <path d="M6.598 5.013a.144.144 0 0 1 .202.134V6.3a.5.5 0 0 0 .5.5c.667 0 2.013.005 3.3.822.984.624 1.99 1.76 2.595 3.876-1.02-.983-2.185-1.516-3.205-1.799a8.74 8.74 0 0 0-1.921-.306 7.404 7.404 0 0 0-.798.008h-.013l-.005.001h-.001L7.3 9.9l-.05-.498a.5.5 0 0 0-.45.498v1.153c0 .108-.11.176-.202.134L2.614 8.254a.503.503 0 0 0-.042-.028.147.147 0 0 1 0-.252.499.499 0 0 0 .042-.028l3.984-2.933zM7.8 10.386c.068 0 .143.003.223.006.434.02 1.034.086 1.7.271 1.326.368 2.896 1.202 3.94 3.08a.5.5 0 0 0 .933-.305c-.464-3.71-1.886-5.662-3.46-6.66-1.245-.79-2.527-.942-3.336-.971v-.66a1.144 1.144 0 0 0-1.767-.96l-3.994 2.94a1.147 1.147 0 0 0 0 1.946l3.994 2.94a1.144 1.144 0 0 0 1.767-.96v-.667z"/>
                                                                                     </svg>
@@ -25945,7 +25939,6 @@
                                                                                 </div>
                                                                                 @else
                                                                                 <a href="/register_komentar">
-
                                                                                     <div id="" class="balaskomen" data-id="balas-{{$row->id}}" style="margin-left: 100px; display:flex;" data-bs-toggle="modal" data-bs-target="#exampleModal">
                                                                                         
                                                                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-reply" viewBox="0 0 16 16">
@@ -25977,14 +25970,34 @@
                                                                         @endauth
                                                                         @endif
                                                                         @foreach ($row->childs as $childs)
-                                                                            
-                                                                        <div class="d-flex">
-                                                                            
-                                                                             <span class="nname-info meta-author" style="margin-right: 9px">
-                                                                              <p class="nice-name" href="#">{{ $row->nama }}</p>
-                                                                            </span>
-                                                                            <p>{{$childs->komentar}}</p>
+
+                                                                        <div class="author-info-wrap" style="margin-left: 20px">
+                                                                            <p class="author-avatar" href="#"><img src="{{asset('profile.jpg')}}" width="120" height="120" alt="Connor" class="avatar avatar-120 wp-user-avatar wp-user-avatar-120 alignnone photo" loading="lazy" /></p>
+                                                                            <div class="is-meta">
+                                                                                <span class="nname-info meta-author">
+                                                                                    <p class="nice-name" href="#">{{ $childs->nama }}</p>
+                                                                                </span>
+                                                                                <div class="" style="display: flex">
+                                                                                    {{$childs->komentar}}
+                                                                                    
+                                                                                </div>
+                                                                                
+                                                                                
+                                                                            </div>
                                                                         </div>
+                                                                        <form action="/komentar/{{$data->id}}" method="post" id="balas-{{$row->id}}" class="mb-3">
+                                                                            @csrf
+                                                                            <input type="hidden" name="parent" value="{{$childs->id}}">
+                                                                            {{-- <input type="text"  name="komentar" class="form-control mt-3 mb-3"  placeholder="Balas Komentar" id=""> --}}
+                                                                            <textarea id="komentar" name="komentar" placeholder="Balas Komentar" cols="45" rows="8" class="mb-3 mt-5" required></textarea>
+
+                                                                            
+                                                                            <input type="submit" class="is-btn header-element" style="margin-top: 8px" value="Balas">
+                    
+
+                                                                        </form>
+                                                                            
+                                                                        
                                                                         @endforeach
                                                                 </div>
                                                             </div>
@@ -26901,6 +26914,15 @@
     <script>
         $(document).ready(function () {
             $('.balaskomen').click(function () {
+                var id = $(this).data('id');
+                console.log(id)
+                $('#'+id).toggle('slide')
+            })
+        })
+    </script>
+    <script>
+        $(document).ready(function () {
+            $('.balaskomen1').click(function () {
                 var id = $(this).data('id');
                 console.log(id)
                 $('#'+id).toggle('slide')
